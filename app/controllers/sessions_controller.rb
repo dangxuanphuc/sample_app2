8b6d168cd2cc755c4e38c7985f34c04eb14180e1
Dang Xuan Phuc <dangxuanphuc2211@gmail.com>
2018-10-06 20:42:31 +0700

    Advanced Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate(params[:session][:password])
       log_in user
+      remember_user
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
@@ -13,7 +14,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
+  end
+
+  private
+
+  def remember_user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
